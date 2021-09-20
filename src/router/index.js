f5bd2904c8224458986093411deeae0b859b7b2d
primagiant <kadekprimagiantmartadinata@gmail.com>
2021-09-20 21:25:01 +0800

    Menambah view dan route pada aplikasi

--- src/router/index.js
+++ src/router/index.js
@@ -14,7 +14,17 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import('../views/Kasir.vue')
-  }
+  },
+  {
+    path: '/about',
+    name: 'About',
+    component: () => import('../views/About.vue')
+  },
+  {
+    path: '/contact',
+    name: 'Contact',
+    component: () => import('../views/Contact.vue')
+  },
 ]
 
 const router = createRouter({
