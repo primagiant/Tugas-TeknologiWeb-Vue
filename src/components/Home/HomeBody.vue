48e4cd8efa6bf8bd387904570d54d9a7660c422a
primagiant <kadekprimagiantmartadinata@gmail.com>
2021-09-21 07:27:36 +0800

    memperbaiki components homebody

--- src/components/Home/HomeBody.vue
+++ src/components/Home/HomeBody.vue
@@ -2,6 +2,11 @@
     <div class="font-quicksand">
         <div class="m-8">
             <h1 class="text-2xl md:text-3xl">Dashboard</h1>
+
+            <div class="bg-indigo-100 p-12 mt-4 rounded-lg">
+                <h3 class="text-4xl text-center">Selamat Datang</h3>
+                <p class="text-center mt-5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur sequi deserunt officiis suscipit id iste aliquam corrupti consectetur est expedita, sint enim, recusandae adipisci magni.</p>
+            </div>
         </div>
     </div>
 </template>
